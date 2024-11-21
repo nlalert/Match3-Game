7ed16168a1cf37b2af880192baed50ed0f3504b1
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-11-21 17:54:06 +0700

    add base UI Layout

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     public BoardManager board;
+    public UIManager uiManager;
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
+    private void Update(){
+        if (Input.GetMouseButtonDown(0)){
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             board.HandleFossilClick(mousePos);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            uiManager.TogglePause();
         }
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject settingsPanel;
+
+    private bool isPaused = false;
+
+    void Start(){
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void OnResumeButtonPressed(){
+        isPaused = false;
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void OnSettingsButtonPressed(){
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void OnSettingsBackButtonPressed(){
+        pausePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void OnMainMenuButtonPressed(){
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void TogglePause(){
+        isPaused = !isPaused;
+        if(isPaused)
+            PauseGame();
+        else
+            OnResumeButtonPressed();
+    }
+
+    public void PauseGame(){
+        isPaused = true;
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void CloseSettings(){
+        settingsPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+}
