25dd7323a898eadf7c0bb4109f7fc60423338a35
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-11-22 21:39:11 +0700

    refactor main menu

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
-{
+public class InputManager : MonoBehaviour{
     public BoardManager board;
     public UIManager uiManager;
 
     private void Update(){
+        // Toggle pause if the ESC key is pressed
         if (Input.GetKeyDown(KeyCode.Escape)){
             uiManager.TogglePause();
         }
-        
+
+        // Process fossil click when left mouse button is clicked
         if (!uiManager.isPaused && Input.GetMouseButtonDown(0)){
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             board.HandleFossilClick(mousePos);
         }
-
     }
 }
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,22 +5,23 @@ public class MainMenuManager : MonoBehaviour {
     public string gameSceneName = "GameStage";
     public GameObject settingsPanel;
 
+    // Load the game scene to start the game
     public void StartGame() {
         SceneManager.LoadScene(gameSceneName);
     }
 
+    // Open the settings panel in the main menu
     public void OpenSettings() {
         settingsPanel.SetActive(true);
     }
 
+    // Close the settings panel in the main menu
     public void CloseSettings() {
         settingsPanel.SetActive(false);
     }
 
+    // Exit the game
     public void ExitGame() {
         Application.Quit();
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
     }
 }
