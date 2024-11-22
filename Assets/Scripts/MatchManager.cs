3e36a3e9ff595a56467558d01163e51194da8523
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-11-22 21:49:09 +0700

    refactor MatchManager

--- Assets/Scripts/MatchManager.cs
+++ Assets/Scripts/MatchManager.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MatchManager : MonoBehaviour{
-
     public BoardManager board;
     public ScoreManager scoreManager;
     public PowerUpManager powerUpManager;
 
-    public List<Fossil> GetMatches(Fossil fossil) {
-        if (fossil.type == FossilType.DNA) {
+    // Find all match of a fossil. (DNA don't match with other DNA)
+    public List<Fossil> GetMatches(Fossil fossil){
+        if (fossil.type == FossilType.DNA){
             return null;
         }
 
@@ -18,40 +18,42 @@ public class MatchManager : MonoBehaviour{
         // Get horizontal matches
         List<Fossil> horizontalMatches = GetHorizontalMatches(fossil);
         if (horizontalMatches.Count >= 3) {
-            foreach (Fossil c in horizontalMatches) {
-                matchedFossils.Add(c);
+            foreach (Fossil fossilElement in horizontalMatches) {
+                matchedFossils.Add(fossilElement);
             }
         }
 
         // Get vertical matches
         List<Fossil> verticalMatches = GetVerticalMatches(fossil);
         if (verticalMatches.Count >= 3) {
-            foreach (Fossil c in verticalMatches) {
-                matchedFossils.Add(c);
+            foreach (Fossil fossilElement in verticalMatches) {
+                matchedFossils.Add(fossilElement);
             }
         }
 
-        // Return the combined list of matches
         return matchedFossils.Count >= 3 ? new List<Fossil>(matchedFossils) : null;
     }
 
-    private List<Fossil> GetHorizontalMatches(Fossil fossil) {
+    // Find all fosiil that horizontal match of a fossil
+    private List<Fossil> GetHorizontalMatches(Fossil fossil){
         List<Fossil> matches = new List<Fossil> { fossil };
 
         // Check left
-        for (int i = fossil.x - 1; i >= 0; i--) {
-            if (board.fossils[i, fossil.y] != null && board.fossils[i, fossil.y].type == fossil.type) {
+        for (int i = fossil.x - 1; i >= 0; i--){
+            if (board.fossils[i, fossil.y] != null && board.fossils[i, fossil.y].type == fossil.type){
                 matches.Add(board.fossils[i, fossil.y]);
-            } else {
+            }
+            else{
                 break;
             }
         }
 
         // Check right
-        for (int i = fossil.x + 1; i < board.width; i++) {
-            if (board.fossils[i, fossil.y] != null && board.fossils[i, fossil.y].type == fossil.type) {
+        for (int i = fossil.x + 1; i < board.width; i++){
+            if (board.fossils[i, fossil.y] != null && board.fossils[i, fossil.y].type == fossil.type){
                 matches.Add(board.fossils[i, fossil.y]);
-            } else {
+            }
+            else{
                 break;
             }
         }
@@ -59,23 +61,26 @@ public class MatchManager : MonoBehaviour{
         return matches;
     }
 
-    private List<Fossil> GetVerticalMatches(Fossil fossil) {
+    // Find all fosiil that vertical match of a fossil
+    private List<Fossil> GetVerticalMatches(Fossil fossil){
         List<Fossil> matches = new List<Fossil> { fossil };
 
         // Check down
-        for (int i = fossil.y - 1; i >= 0; i--) {
-            if (board.fossils[fossil.x, i] != null && board.fossils[fossil.x, i].type == fossil.type) {
+        for (int i = fossil.y - 1; i >= 0; i--){
+            if (board.fossils[fossil.x, i] != null && board.fossils[fossil.x, i].type == fossil.type){
                 matches.Add(board.fossils[fossil.x, i]);
-            } else {
+            }
+            else{
                 break;
             }
         }
 
         // Check up
-        for (int i = fossil.y + 1; i < board.height; i++) {
-            if (board.fossils[fossil.x, i] != null && board.fossils[fossil.x, i].type == fossil.type) {
+        for (int i = fossil.y + 1; i < board.height; i++){
+            if (board.fossils[fossil.x, i] != null && board.fossils[fossil.x, i].type == fossil.type){
                 matches.Add(board.fossils[fossil.x, i]);
-            } else {
+            }
+            else{
                 break;
             }
         }
@@ -83,7 +88,8 @@ public class MatchManager : MonoBehaviour{
         return matches;
     }
 
-    public void DestroyMatches(List<Fossil> matches) {
+    // Destroy matched fossils, update score, and handle power-up
+    public void DestroyMatches(List<Fossil> matches){
         if (matches == null || matches.Count < 3) return;
 
         PlayRandomMatchSound();
@@ -91,37 +97,41 @@ public class MatchManager : MonoBehaviour{
 
         Fossil powerUpFossil = null;
 
-        if (matches.Count >= 4) {
-            powerUpFossil = powerUpManager.HandlePowerUpCreation(matches); // Create a LineClear power-up
+        // Handle power-up creation for larger matches
+        if (matches.Count >= 4){
+            powerUpFossil = powerUpManager.HandlePowerUpCreation(matches);
         }
 
-        foreach (Fossil fossil in matches) {
-            if (fossil == powerUpFossil) {
-                continue; // Skip destroying the central fossil
+        foreach (Fossil fossil in matches){
+            // Skip destroying center fossil that use power-up
+            if (fossil == powerUpFossil){
+                continue;
             }
 
-            if (fossil.powerUpType != PowerUpType.None) {
-                powerUpManager.ActivatePowerUp(fossil); // Activate any other power-ups
+            // Activate and destroy other power-up
+            if (fossil.powerUpType != PowerUpType.None){
+                powerUpManager.ActivatePowerUp(fossil);
             }
+
             board.DestroyFossil(fossil);
         }
     }
 
+    // Play random match sound
     private void PlayRandomMatchSound(){
         int randomSound = Random.Range(0, 4);
-        switch (randomSound)
-        {
+        switch (randomSound){
             case 0:
-                AudioManager.Instance.PlaySound(AudioManager.Instance.match1); // Play match sound
+                AudioManager.Instance.PlaySound(AudioManager.Instance.match1);
                 break;
             case 1:
-                AudioManager.Instance.PlaySound(AudioManager.Instance.match2); // Play match sound
+                AudioManager.Instance.PlaySound(AudioManager.Instance.match2);
                 break;
             case 2:
-                AudioManager.Instance.PlaySound(AudioManager.Instance.match3); // Play match sound
+                AudioManager.Instance.PlaySound(AudioManager.Instance.match3);
                 break;
             default:
-                AudioManager.Instance.PlaySound(AudioManager.Instance.match4); // Play match sound
+                AudioManager.Instance.PlaySound(AudioManager.Instance.match4);
                 break;
         }
     }
